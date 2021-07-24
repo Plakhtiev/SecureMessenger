73afe48f264822a1001233ac5aefb0dddf707147
Plakhtiev <monatkotata@gmail.com>
2021-07-24 13:09:44 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+
+#include <QApplication>
+
+#include "chatdialog.h"
+
+#include <QtCore/QSettings>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    ChatDialog dialog;
+    dialog.show();
+    return app.exec();
+}
